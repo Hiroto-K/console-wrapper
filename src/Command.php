f74e466e582dfe09b1462f970b7bc5fa144b3030
Hiroto Kitazawa <hiro.yo.yo1610@gmail.com>
2018-06-14 00:07:07 +0900

    Create Command class in "HirotoK\ConsoleWrapper"

---
+++ src/Command.php
@@ -0,0 +1,12 @@
+<?php
+namespace HirotoK\ConsoleWrapper;
+use Symfony\Component\Console\Command\Command as SymfonyCommand;
+
+/**
+ * Class Command
+ * @package HirotoK\ConsoleWrapper
+ */
+abstract class Command extends SymfonyCommand
+{
+
+}
