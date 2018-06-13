e9e57e0283f43a00fce23371532f2cf263f2fe1c
Hiroto Kitazawa <hiro.yo.yo1610@gmail.com>
2018-06-14 00:10:29 +0900

    Fix the coding style

--- src/Command.php
+++ src/Command.php
@@ -1,12 +1,23 @@
 <?php
+
+/*
+ * This file is part of console-wrapper.
+ *
+ * (c) Hiroto Kitazawa <hiro.yo.yo1610@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace HirotoK\ConsoleWrapper;
+
 use Symfony\Component\Console\Command\Command as SymfonyCommand;
 
 /**
- * Class Command
+ * Class Command.
+ *
  * @package HirotoK\ConsoleWrapper
  */
 abstract class Command extends SymfonyCommand
 {
-
 }
