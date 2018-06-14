c34dfe921597b636c4a6c12933ed19677a2e8128
Hiroto Kitazawa <hiro.yo.yo1610@gmail.com>
2018-06-14 20:41:35 +0900

    Create tests

--- tests/CommandTest.php
+++ tests/CommandTest.php
@@ -1,5 +1,6 @@
 <?php
 namespace HirotoK\ConsoleWrapper\Tests;
+use HirotoK\ConsoleWrapper\Command;
 use PHPUnit\Framework\TestCase;
 
 /**
@@ -7,5 +8,22 @@ use PHPUnit\Framework\TestCase;
  * @package HirotoK\ConsoleWrapper\Tests
  */
 class CommandTest extends TestCase {
+
+    public function testName(){
+        $class = new class extends Command{
+            protected $name = "test:name";
+        };
+
+        $this->assertSame("test:name", $class->getName());
+    }
+
+    public function testDescription(){
+        $class = new class extends Command{
+            protected $name = "example";
+            protected $description = "This is a test description.";
+        };
+
+        $this->assertSame("This is a test description.", $class->getDescription());
+    }
 
 }
