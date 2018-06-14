4f6880426944f51a50d2baa392d312335b8822ee
Hiroto Kitazawa <hiro.yo.yo1610@gmail.com>
2018-06-14 20:36:57 +0900

    Create CommandTest class

---
+++ tests/CommandTest.php
@@ -0,0 +1,11 @@
+<?php
+namespace HirotoK\ConsoleWrapper\Tests;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class CommandTest
+ * @package HirotoK\ConsoleWrapper\Tests
+ */
+class CommandTest extends TestCase {
+
+}
