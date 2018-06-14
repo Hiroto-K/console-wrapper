fe8c170b52e9fde7079a9a2cfeffa37d39b13489
Hiroto Kitazawa <hiro.yo.yo1610@gmail.com>
2018-06-14 19:30:27 +0900

    Create LogicException class

---
+++ src/Exception/LogicException.php
@@ -0,0 +1,10 @@
+<?php
+namespace HirotoK\ConsoleWrapper\Exception;
+
+/**
+ * Class LogicException
+ * @package HirotoK\ConsoleWrapper\Exception
+ */
+class LogicException extends \LogicException
+{
+}
