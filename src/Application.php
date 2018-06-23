526e34be7adccd46d6362d5d9fdcbbd2435d4f0d
Hiroto Kitazawa <hiro.yo.yo1610@gmail.com>
2018-06-23 10:31:58 +0900

    Create Application class

---
+++ src/Application.php
@@ -0,0 +1,11 @@
+<?php
+namespace HirotoK\ConsoleWrapper;
+use Symfony\Component\Console\Application as SymfonyApplication;
+
+/**
+ * Class Application
+ * @package HirotoK\ConsoleWrapper
+ */
+class Application extends SymfonyApplication
+{
+}
