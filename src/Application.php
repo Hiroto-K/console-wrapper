ab0cdf30b68ecf8857bbf3bd4afdbe5dd3ac07dc
Hiroto Kitazawa <hiro.yo.yo1610@gmail.com>
2018-06-23 10:32:09 +0900

    Fix the coding style

--- src/Application.php
+++ src/Application.php
@@ -1,9 +1,21 @@
 <?php
+
+/*
+ * This file is part of console-wrapper.
+ *
+ * (c) Hiroto Kitazawa <hiro.yo.yo1610@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace HirotoK\ConsoleWrapper;
+
 use Symfony\Component\Console\Application as SymfonyApplication;
 
 /**
- * Class Application
+ * Class Application.
+ *
  * @package HirotoK\ConsoleWrapper
  */
 class Application extends SymfonyApplication
