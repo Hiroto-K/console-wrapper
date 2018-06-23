487ba1d1dcbe43563d16966fbd548efca3c7dd28
Hiroto Kitazawa <hiro.yo.yo1610@gmail.com>
2018-06-23 11:02:09 +0900

    Fix the coding style

--- tests/ApplicationTest.php
+++ tests/ApplicationTest.php
@@ -1,4 +1,14 @@
 <?php
+
+/*
+ * This file is part of console-wrapper.
+ *
+ * (c) Hiroto Kitazawa <hiro.yo.yo1610@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace HirotoK\ConsoleWrapper\Tests;
 
 use HirotoK\ConsoleWrapper\Application;
@@ -6,24 +16,26 @@ use PHPUnit\Framework\TestCase;
 use Psr\Log\NullLogger;
 
 /**
- * Class ApplicationTest
+ * Class ApplicationTest.
+ *
  * @package HirotoK\ConsoleWrapper\Tests
  */
-class ApplicationTest extends TestCase{
-
-    public function testLoggerNotSetsReturnNullLogger(){
-        $application = new Application;
+class ApplicationTest extends TestCase
+{
+    public function testLoggerNotSetsReturnNullLogger()
+    {
+        $application = new Application();
 
         $this->assertInstanceOf(NullLogger::class, $application->logger());
     }
 
-    public function testLoggerSets(){
-        $application = new Application;
+    public function testLoggerSets()
+    {
+        $application = new Application();
 
         $loggerMock = $this->createMock(\Psr\Log\LoggerInterface::class);
         $application->setLogger($loggerMock);
 
         $this->assertSame($loggerMock, $application->logger());
     }
-
 }
