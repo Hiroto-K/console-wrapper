866c1c85d4c760a16eb7e1dfc3676a0f041ddbfb
Hiroto Kitazawa <hiro.yo.yo1610@gmail.com>
2018-06-23 10:56:54 +0900

    Create $nullLogger property

--- src/Application.php
+++ src/Application.php
@@ -26,16 +26,25 @@ class Application extends SymfonyApplication implements LoggerAwareInterface
     use LoggerAwareTrait;
 
     /**
+     * @var \Psr\Log\NullLogger
+     */
+    protected $nullLogger;
+
+    /**
      * Get logger instance.
      *
      * @return \Psr\Log\LoggerInterface
      */
     public function logger()
     {
-        if ($this->logger) {
+        if (isset($this->logger)) {
             return $this->logger;
         }
 
-        return new NullLogger();
+        if (!isset($this->nullLogger)){
+            $this->nullLogger = new NullLogger();
+        }
+
+        return $this->nullLogger;
     }
 }
