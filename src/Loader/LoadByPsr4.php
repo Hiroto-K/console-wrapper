3fdf8b8c990a9f0c6c731b244e8d366faa46931b
Hiroto Kitazawa <hiro.yo.yo1610@gmail.com>
2018-06-23 12:59:12 +0900

    Fix the coding style

--- src/Loader/LoadByPsr4.php
+++ src/Loader/LoadByPsr4.php
@@ -1,12 +1,23 @@
 <?php
+
+/*
+ * This file is part of console-wrapper.
+ *
+ * (c) Hiroto Kitazawa <hiro.yo.yo1610@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace HirotoK\ConsoleWrapper\Loader;
 
 /**
- * Class LoadByPsr4
+ * Class LoadByPsr4.
+ *
  * @package HirotoK\ConsoleWrapper\Loader
  */
-class LoadByPsr4{
-
+class LoadByPsr4
+{
     /**
      * Namespace prefix.
      *
