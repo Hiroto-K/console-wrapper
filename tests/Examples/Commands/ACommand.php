200bad1bd9912aa69b8d226e1d9fc219da197304
Hiroto Kitazawa <hiro.yo.yo1610@gmail.com>
2018-12-07 12:45:47 +0900

    Add tests to Application::loadByPsr4() method

---
+++ tests/Examples/Commands/ACommand.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * This file is part of console-wrapper.
+ *
+ * (c) Hiroto Kitazawa <hiro.yo.yo1610@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace HirotoK\ConsoleWrapper\Tests\Examples\Commands;
+
+use HirotoK\ConsoleWrapper\Command;
+
+class ACommand extends Command
+{
+    protected $name = 'a';
+}
